2a2d7563066de4672374cc669db61588dbf68426
hanshanghao <shanghao.han28@gmail.com>
2026-08-24 16:42:21 +0800

    Add Firestore rules reference comment to app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+/*
+rules_version = '2';
+
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if false;
+    }
+  }
+}
+*/
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
   getAuth,
